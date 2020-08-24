c16f16287c5b27120b123ae34226d78259584bd2
Matthew Horan <matt@matthoran.com>
2020-08-23 23:21:31 -0400

    Rename $TMPDIR variable
    This directory is used for mkfifo, so be explicit.

--- backup.sh
+++ backup.sh
@@ -6,15 +6,15 @@ mysql_defaults_file=$(mktemp)
 echo "[client]
 password=$mysql_root_password" > "$mysql_defaults_file"
 
-TMPDIR=$(mktemp -d)
-mkfifo -m 600 "$TMPDIR/fifo"
+fifo_tmpdir=$(mktemp -d)
+mkfifo -m 600 "$fifo_tmpdir/fifo"
 
-mysql --defaults-file="$mysql_defaults_file" < "$TMPDIR/fifo" &
+mysql --defaults-file="$mysql_defaults_file" < "$fifo_tmpdir/fifo" &
 
 # keep the pipe open after we write to it
-exec 3> "$TMPDIR/fifo"
+exec 3> "$fifo_tmpdir/fifo"
 echo "BACKUP STAGE START;
-BACKUP STAGE BLOCK_COMMIT;" > "$TMPDIR/fifo"
+BACKUP STAGE BLOCK_COMMIT;" > "$fifo_tmpdir/fifo"
 
 format=%Y%m%d
 DATE=$(date +$format)
@@ -22,9 +22,9 @@ gcloud compute disks snapshot "$DISK_NAME" --zone="$DISK_ZONE" \
   --snapshot-names="mariadb-snapshot-$DATE"
 
 echo "BACKUP STAGE END;
-exit" > "$TMPDIR/fifo"
+exit" > "$fifo_tmpdir/fifo"
 
-rm -r "$TMPDIR"
+rm -r "$fifo_tmpdir"
 rm "$mysql_defaults_file"
 
 archives=$(gcloud compute snapshots list \
